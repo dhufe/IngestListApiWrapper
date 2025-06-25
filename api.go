c22128ab9e2c48386ca3ac909ed1ff78ccb087a0
Daniel Hufschläger <daniel.hufschlaeger@blha.brandenburg.de>
2025-06-25 09:25:06 +0200

    Inital commit.

---
+++ api.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	//	"net"
+	"fmt"
+	"net/http"
+	"os"
+	"path/filepath"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
+
+func getDefaultResponse(c *gin.Context) {
+	c.String(http.StatusOK, fmt.Sprintf(DEFAULT_RESPONSE, VERSION))
+}
+
+func identifyFile(c *gin.Context) {
+}
+
+func uploadFile(c *gin.Context) {
+	file, err := c.FormFile("file")
+	// no file received
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"message": "no file received",
+		})
+		return
+	}
+	// generate unique file name for storing
+	filename := uuid.New().String() + "_" + file.Filename
+	fileStorePath := filepath.Join(FILE_STORE_PATH, filename)
+	err = c.SaveUploadedFile(file, fileStorePath)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"message": "unable to save file",
+		})
+		return
+	}
+	defer os.Remove(fileStorePath)
+}
