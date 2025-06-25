81bf075b0385356147fc847d92e45ac31b075b9e
Daniel Hufschläger <daniel@hufschlaeger.net>
2025-06-25 17:48:58 +0200

    Adding proper config management.

---
+++ config.go
@@ -0,0 +1,168 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"gopkg.in/yaml.v2"
+)
+
+type Config struct {
+	Server struct {
+		Host    string `yaml:"host"`
+		Port    string `yaml:"port"`
+		Timeout struct {
+			// Server is the general server timeout to use
+			// for graceful shutdowns
+			Server time.Duration `yaml:"server"`
+
+			// Write is the amount of time to wait until an HTTP server
+			// write opperation is cancelled
+			Write time.Duration `yaml:"write"`
+
+			// Read is the amount of time to wait until an HTTP server
+			// read operation is cancelled
+			Read time.Duration `yaml:"read"`
+
+			// Read is the amount of time to wait
+			// until an IDLE HTTP session is closed
+			Idle time.Duration `yaml:"idle"`
+		} `yaml:"timeout"`
+	} `yaml:"server"`
+}
+
+func NewConfig(configPath string) (*Config, error) {
+	// Create config structure
+	config := &Config{}
+
+	// Open config file
+	file, err := os.Open(configPath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	// Init new YAML decode
+	d := yaml.NewDecoder(file)
+
+	// Start YAML decoding from file
+	if err := d.Decode(&config); err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
+
+func ValidateConfigPath(path string) error {
+	s, err := os.Stat(path)
+	if err != nil {
+		return err
+	}
+	if s.IsDir() {
+		return fmt.Errorf("'%s' is a directory, not a normal file", path)
+	}
+	return nil
+}
+
+func ParseFlags() (string, error) {
+	// String that contains the configured configuration path
+	var configPath string
+
+	// Set up a CLI flag called "-config" to allow users
+	// to supply the configuration file
+	flag.StringVar(&configPath, "config", "./config.yml", "path to config file")
+
+	// Actually parse the flags
+	flag.Parse()
+
+	// Validate the path first
+	if err := ValidateConfigPath(configPath); err != nil {
+		return "", err
+	}
+
+	// Return the configuration path
+	return configPath, nil
+}
+
+func NewRouter() *gin.Engine {
+	router := http.NewServeMux()
+	//router.MaxMultipartMemory = 3000 << 20
+	/*
+		router := gin.Default()
+		router.MaxMultipartMemory = 3000 << 20 // 3 GiB
+		// Allow cors to integrate Borg in other applications.
+		router.ForwardedByClientIP = true
+		router.SetTrustedProxies([]string{"*"})
+		corsConfig := cors.DefaultConfig()
+		corsConfig.AllowOrigins = []string{"*"}
+		corsConfig.AllowHeaders = []string{"Origin", "Content-Type"}
+		corsConfig.AllowMethods = []string{"GET", "POST"}
+		// It's important that the cors configuration is used before declaring the routes.
+		router.Use(cors.New(corsConfig))
+		router.GET("api", getDefaultResponse)
+
+		router.POST("/api/upload", uploadFile)
+		router.GET("/api/identify", identifyFile)
+		router.Run(":8080")
+	*/
+	return router
+}
+
+func (config Config) Run() {
+	// Set up a channel to listen to for interrupt signals
+	runChan := make(chan os.Signal, 1)
+
+	// Set up a context to allow for graceful server shutdowns in the event
+	// of an OS interrupt (defers the cancel just in case)
+	ctx, cancel := context.WithTimeout(
+		context.Background(),
+		config.Server.Timeout.Server,
+	)
+	defer cancel()
+
+	// Define server options
+	server := &http.Server{
+		Addr:         config.Server.Host + ":" + config.Server.Port,
+		Handler:      NewRouter(),
+		ReadTimeout:  config.Server.Timeout.Read * time.Second,
+		WriteTimeout: config.Server.Timeout.Write * time.Second,
+		IdleTimeout:  config.Server.Timeout.Idle * time.Second,
+	}
+
+	// Handle ctrl+c/ctrl+x interrupt
+	signal.Notify(runChan, os.Interrupt, syscall.SIGTSTP)
+
+	// Alert the user that the server is starting
+	log.Printf("Server is starting on %s\n", server.Addr)
+
+	// Run the server on a new goroutine
+	go func() {
+		if err := server.ListenAndServe(); err != nil {
+			if err == http.ErrServerClosed {
+				// Normal interrupt operation, ignore
+			} else {
+				log.Fatalf("Server failed to start due to err: %v", err)
+			}
+		}
+	}()
+
+	// Block on this channel listening for those previously defined syscalls assign
+	// to variable so we can let the user know why the server is shutting down
+	interrupt := <-runChan
+
+	// If we get one of the pre-prescribed syscalls, gracefully terminate the server
+	// while alerting the user
+	log.Printf("Server is shutting down due to %+v\n", interrupt)
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("Server was unable to gracefully shutdown due to err: %+v", err)
+	}
+}
